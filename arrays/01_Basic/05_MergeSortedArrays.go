e3679996b3a5204f8512753fd34cee0882645e00
ZeeroIndex <nidhi.singh@indicsoft.com>
2024-12-14 00:33:05 +0530

    update

--- arrays/01_Basic/05_MergeSortedArrays.go
+++ arrays/01_Basic/05_MergeSortedArrays.go
@@ -25,9 +25,13 @@ Constraints:
 - The function should solve the problem in linear time (O(n + m)), where n and m are the lengths of the two arrays.
 - The solution should not use any additional space beyond the output array.
 */
+package main
+
+import "fmt"
+
 func main() {
-	arr1 := []int{1, 3, 5, 7}
-	arr2 := []int{2, 4, 6, 8}
+	arr1 := []int{1, 4, 5, 7}
+	arr2 := []int{0, 2, 6, 8, 9}
 
 	mergedArr := mergeSortedArrays(arr1, arr2)
 	fmt.Println("Merged Array:", mergedArr)
@@ -35,5 +39,29 @@ func main() {
 
 func mergeSortedArrays(arr1 []int, arr2 []int) []int {
 	merged := []int{}
+	i, j := 0, 0
 
+	for i < len(arr1) && j < len(arr2) {
+		// fmt.Println("arr1", arr1[i])
+		// fmt.Println("arr2", arr2[j])
+		if arr1[i] < arr2[j] {
+			merged = append(merged, arr1[i])
+			i++
+		} else {
+			merged = append(merged, arr2[j])
+			j++
+		}
+	}
+
+	for i < len(arr1) {
+		merged = append(merged, arr1[i])
+		i++
+	}
+
+	for j < len(arr2) {
+		merged = append(merged, arr2[j])
+		j++
+	}
+
+	return merged
 }
